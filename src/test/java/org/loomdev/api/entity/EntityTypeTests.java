47ce3ff5170b29a38cd3bb258d287ab82a37a37e
Jeroen Verwimp <jeroen@verwimp.me>
2020-07-19 00:43:41 +0200

    Added entity ids to EntityType

--- src/test/java/org/loomdev/api/entity/EntityTypeTests.java
+++ src/test/java/org/loomdev/api/entity/EntityTypeTests.java
@@ -4,22 +4,32 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.loomdev.api.block.BlockTypes;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class EntityTypeTests {
 
     @Test
     void doEnumValuesMatchRegistry() {
-        List<String> current = Arrays.stream(EntityType.values()).map(Enum::toString).collect(Collectors.toList());
-        List<String> missing =  Registry.ENTITY_TYPE.getIds().stream()
-                .map(Identifier::getPath)
-                .map(String::toUpperCase)
-                .filter(value -> !current.contains(value))
-                .collect(Collectors.toList());
-        Assertions.assertTrue(missing.isEmpty(), "Missing entity types: \n" + String.join(",\n", missing));
+        Map<String, String> current = Arrays.stream(EntityType.values()).collect(Collectors.toMap(Enum::name, EntityType::getId));
+        Map<String, String> missing = new HashMap<>();
+        List<String> versionMismatch = new ArrayList<>();
+
+        for (Identifier id : Registry.ENTITY_TYPE.getIds()) {
+            String name = id.getPath().toUpperCase();
+            if (!current.containsKey(name)) {
+                missing.put(name, id.toString());
+            } else if (!current.get(name).equals(id.toString())) {
+                versionMismatch.add("Wrong id:! " + name + " has " + current.get(name) + " but should be " + id.toString());
+            }
+        }
+
+        List<Map.Entry<String, String>> missingSorted = new ArrayList<>(missing.entrySet());
+        missingSorted.sort(Map.Entry.comparingByValue());
+        Assertions.assertTrue(missing.isEmpty(), "Missing entity types: \n" + String.join(",\n", missingSorted.stream().map(kv -> kv.getKey() + "(\"" + kv.getValue() + "\")").collect(Collectors.toList())));
+        Assertions.assertTrue(versionMismatch.isEmpty(), String.join("\n", versionMismatch));
     }
 
 }
