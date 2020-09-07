684251777fa5f401e58cbb8f52b37c02daf5b0ef
Jeroen Verwimp <jeroen@verwimp.me>
2020-09-07 22:40:35 +0200

    Update to 1.16.3-rc1

--- src/main/java/org/loomdev/api/ApiVersion.java
+++ src/main/java/org/loomdev/api/ApiVersion.java
@@ -6,21 +6,38 @@ import java.util.stream.Collectors;
 
 public enum ApiVersion {
 
-    UNKNOWN("unknown"),
-    v1_16_1R1("1.16.1-R1"),
-    v1_16_2R1("1.16.2-R1");
+    UNKNOWN("unknown", false),
+    v1_16_1R1("1.16.1-R1", false),
+    v1_16_2R1("1.16.2-R1", false),
+    v1_16_3RC1("1.16.3-RC1", true);
 
     public static final ApiVersion LATEST = values()[ApiVersion.values().length - 1];
+    public static final ApiVersion LATEST_RELEASE = Arrays.stream(values())
+            .filter(v -> !v.isSnapshot())
+            .reduce((a, b) -> b).get();
+    public static final ApiVersion LATEST_SNAPSHOT = Arrays.stream(values())
+            .filter(ApiVersion::isSnapshot)
+            .reduce((a, b) -> b).get();
 
     private static final Map<String, ApiVersion> NAME_VERSION_MAP = Arrays.stream(values()).collect(Collectors.toMap(ApiVersion::getName, apiVersion -> apiVersion));
     private final String name;
+    private final boolean snapshot;
 
-    ApiVersion(String name) {
+    ApiVersion(String name, boolean snapshot) {
         this.name = name;
+        this.snapshot = snapshot;
     }
 
     public String getName() {
         return name;
+    }
+
+    public boolean isSnapshot() {
+        return snapshot;
+    }
+
+    public boolean isLatest() {
+        return this == LATEST;
     }
 
     public boolean isNewerThan(ApiVersion other) {
